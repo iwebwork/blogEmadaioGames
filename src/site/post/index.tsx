2e7a7e70de03d84a85d9d796af2efed37b2e30ad
Igor <igor.silva@supermercadosbh.com.br>
2025-03-13 09:35:08 -0300

    Spin adicionado

--- src/site/post/index.tsx
+++ src/site/post/index.tsx
@@ -1,4 +1,4 @@
-import { Row } from "antd";
+import { Flex, Spin } from "antd";
 import React, { Suspense, useState } from "react";
 import { useParams } from "react-router";
 import noticias, { IPost, routsPostsNoticias } from "../../data/noticias";
@@ -7,10 +7,17 @@ const PostView: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const [posts] = useState<IPost[]>(noticias);
 
-  const Component = routsPostsNoticias(posts.find(post => post.id === id)?.post || "naoEncontrado");
+  const Component =
+    routsPostsNoticias(posts.find(post => post.id === id)?.post || "naoEncontrado")
 
   return (
-    <Suspense fallback={<Row>Carregando ....</Row>}>
+    <Suspense fallback={
+      <Flex justify="center">
+        <Spin tip="Loading" size="large">
+          Loading...
+        </Spin>
+      </Flex>
+    }>
       <Component />
     </Suspense>
   )
