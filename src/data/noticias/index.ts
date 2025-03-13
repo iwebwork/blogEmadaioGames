4acb41aad7ff1483d02afb9208053f300a64b399
Igor <igor.silva@supermercadosbh.com.br>
2025-03-13 09:38:07 -0300

    Trabalhar com GUIDs

--- src/data/noticias/index.ts
+++ src/data/noticias/index.ts
@@ -21,13 +21,13 @@ export interface IPost {
 
 const noticias: IPost[] = [
   {
-    id: '1',
+    id: 'f3cce9aa-c8cd-4b26-bf31-5958d83f4027',
     post: 'primeiroPost',
     title: `O vento sopra suave, folhas dançam, a tarde se despede, estrelas logo brilharão.`,
     date: date
   },
   {
-    id: '2',
+    id: 'a3e193a4-95f8-42b0-bb5a-ab409d6e0f17',
     post: 'forzaPS5',
     title: `Preparem os motores! Forza Horizont 5,está prestes a acelerar no Playstation 5`,
     date: '04/02/2025'
