9551f77829a4b99d9192b66ceb5a22c47501a83b
iwebwork <iwebwork.adm@gmail.com>
2025-03-13 21:40:30 -0300

    Ajuste remover SIdeBar de Anuncios do AdsTerra

--- src/site/index.tsx
+++ src/site/index.tsx
@@ -4,20 +4,20 @@ import LayoutViewUi from "../ui/layout"
 import NoticiasView from "./noticias"
 import PostView from "./post"
 
-const Sider: React.FC = () => {
-  return (
-    <Flex justify={'center'}>
-      <Col span={18}>
-        < div id="container-03e4cb914ac639fadad951bcf7c1b7f2" />
-      </Col>
-    </Flex >
-  )
-}
+// const Sider: React.FC = () => {
+//   return (
+//     <Flex justify={'center'}>
+//       <Col span={18}>
+//         < div id="container-03e4cb914ac639fadad951bcf7c1b7f2" />
+//       </Col>
+//     </Flex >
+//   )
+// }
 
 const SiteView: React.FC = () => {
   return (
     <LayoutViewUi
-      SiderChildrenRight={<Sider />}
+    // SiderChildrenRight={<Sider />}
     // SiderChildrenLeft={<Sider />}
     >
       <Routes>
