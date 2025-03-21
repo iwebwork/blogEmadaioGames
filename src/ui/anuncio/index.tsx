2e5f5d5ce79b382f51665a0715bde719138f2050
Igor <igor.silva@supermercadosbh.com.br>
2025-03-21 15:45:57 -0300

    ajuste

--- src/ui/anuncio/index.tsx
+++ src/ui/anuncio/index.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren, useRef, useState } from 'react';
+import React, { PropsWithChildren, useRef } from 'react';
 
 const Anuncio: React.FC<PropsWithChildren> = ({ children }) => {
   const botaoAnuncio = useRef<HTMLAnchorElement>(null);
@@ -14,7 +14,6 @@ const Anuncio: React.FC<PropsWithChildren> = ({ children }) => {
       'toolbar=0,location=0,directories=0,status=0,menubar=0,scrollbars=0,resizable=0,width=1,height=1,left=500,top=500');
 
   }
-
 
   return (
     <>
