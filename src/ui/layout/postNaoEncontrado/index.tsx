f78e0c0d8e7074082290f61b070d8af7583ba8df
Igor <igor.silva@supermercadosbh.com.br>
2025-04-02 09:34:41 -0300

    ajustes

--- src/ui/layout/postNaoEncontrado/index.tsx
+++ src/ui/layout/postNaoEncontrado/index.tsx
@@ -1,10 +1,11 @@
+import { Flex } from "antd";
 import React from "react";
 
 const naoEncontrado: React.FC = () => {
   return (
-    <>
+    <Flex justify="center">
       Post não foi encontrado
-    </>
+    </Flex>
   )
 }
 
